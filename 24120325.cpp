83dcf51d3b79819fb4ac0288e8e74898efaccc4c
nang6264 <24120325@student.hcmus.edu.vn>
2025-12-19 21:19:41 +0700

    update tuần 9

--- 24120325.cpp
+++ 24120325.cpp
@@ -1,78 +1,151 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<fstream>
 #include<string>
 #include<sstream>
-
-
+#include<map>
+#include<set>
 using namespace std;
+struct CPlayer;
 
-class CDate {
-	private:
-	int m_Day;
-	int m_Month;
-	int m_Year;
-public:
-	CDate() {}
-	CDate(int day, int month, int year) : m_Day(day), m_Month(month), m_Year(year) {}
-	friend ostream& operator<<(ostream& os, const CDate& date) {
-		os << date.m_Day << "/" << date.m_Month << "/" << date.m_Year;
-	}
-	friend istream& operator>>(istream& is, CDate& date) { // nhap du lieu tu file  dang: 03/01/2006
-		string str;
-		is >> str;
-		date.m_Day = stoi(str.substr(0, 2));
-		date.m_Month = stoi(str.substr(3, 2));
-		date.m_Year = stoi(str.substr(6, 4));
-		return is;
-	}
+void Trim
+class IPlayerState {
+    public:
+    virtual void ApplyStats(CPlayer* player)=0;
+    virtual string GetStateName()=0;
+    virtual ~IPlayerState() {}
 };
-void Trim(string& s) {
-	if (!s.empty() && s[0] == ' ') s.erase(0, 1);
+class CPlayer {
+    private: 
+    string m_ID;
+    string m_Name;
+    int m_Health;
+
+    int m_BaseSpike, m_BaseMovement, m_BaseSet;
+
+    int m_Spike, m_Movement, m_Set;
+
+    vector<string> m_Positions;
+    IPlayerState* m_pState;
+    public:
+CPlayer(): m_State(nullptr){}
+~CPlayer();
+    void setHealth(int h) { m_Health = h; }
+    int getHealth() { return m_Health; }
+    int getTotalStats() { return m_Spike + m_Movement + m_Set; }
+    vector<string> getPositions() { return m_Positions; }
+    string getName() { return m_Name; }
+    string getID() { return m_ID; }
+
+    void setEffectiveStats(int spike, int movement, int set) {
+        m_Spike = spike;
+        m_Movement = movement;
+        m_Set = set;
+    }
+    int getBaseSpike() { return m_BaseSpike; }
+    int getBaseMovement() { return m_BaseMovement; }
+    int getBaseSet() { return m_BaseSet; }
+
+
+    void Input (istream& in);
+    void UpdateState();
+    void UpdatePlayerStatsBasedOnHealth();
+    void CanPlayPosition(string position);
+    void PrintInfo(ostream& out);
+
+};
+
+class roCStngState: public IPlayerState{
+    public: 
+    void ApplyStats(CPlayer* player) override {
+        player->setEffectiveStats(
+            player->getBaseSpike()+10,
+            player->getBaseMovement()+10,
+            player->getBaseSet()+10 
+        );
+    }
+    string GetStateName() override {
+        return "Strong";
+    }
+};
+
+class CNormalState: public IPlayerState{
+    public: 
+    void ApplyStats(CPlayer* player) override {
+        player->setEffectiveStats(
+            player->getBaseSpike()-10,
+            player->getBaseMovement()-10,
+            player->getBaseSet() -10
+        );
+    }
+    string GetStateName() override {
+        return "Tired";
+    }
+};
+
+CPlayer::~CPlayer() {
+    if (m_pState!= nullptr) {
+        delete m_pState;
+        m_pState = nullptr;
+    }
 }
-class CCatologue {
-private:
-	string m_ID; // ma tai lieu
-	string m_Title; // tieu de tai lieu
-	string m_Author; // tac gia
-	int m_Count; // so lan muon
-public:
-	CCatologue() : m_ID(""), m_Title(""), m_Author(""), m_Count(0) {}
-	virtual ~CCatologue() {}
-	void setID(const string& id) { m_ID = id; }
-	string getID() const { return m_ID; }
-	string getAuthor() {
-		return m_Author;
-	}
-	virtual void Input(stringstream &ss) {
-		string tempCount;
-		getline(ss, m_Title, ',');
-		Trim(m_Title);
-		getline(ss, m_Author, ',');
-		Trim(m_Author);
-		getline(ss, tempCount, ',');
-		Trim(tempCount);
-		m_Count = stoi(tempCount);
-	}
-	virtual int GetYear() = 0;
-	virtual string GetType() = 0;
-	virtual void Output(ostream& os) {
-		os << "ID: " << m_ID << ", Title: " << m_Title << ", Author: " << m_Author << ", Count: " << m_Count;
-	}
 
-};
-class CPaper: public CCatologue {
-private:
-	string m_Journal; // ten tap chi
-	int m_Year; // nam xuat ban
-};
-class CBook: public CCatologue {
-	string m_Publisher; // nha xuat ban
-	int m_Year; // nam xuat ban
-	int m_Version; // phien ban
-};
-class CThesis : public CCatologue {
-	string m_School; // truong dai hoc
-	int m_Year; // nam bao ve
-	string m_FAculty; // khoa
-};
+void CPlayer::UpdateState() {
+    if (m_pState != nullptr) {
+        delete m_pState;
+        m_pState = nullptr;
+    }
+    if (m_Health >= 90) {
+        m_pState = new roCStngState();
+    } else if (m_Health > 50) {
+        m_State = new CNormalState();
+    } else {
+        m_State = new CTiredState();
+    }
+}
+
+void CPlayer::UpdatePlayerStatsBasedOnHealth() {
+    UpdateState();
+    if (m_pState != nullptr) {
+        m_pState->ApplyStats(this);
+    }
+}
+
+void CPlayer:: Input(istream&in){
+    // đầu vào : 001 Kerri Walsh Jennings 90 85 80 95 OH S
+    // 1. Đọc ID.
+    // 2. Đọc Name: Vì Name có khoảng trắng, cần đọc từng từ cho đến khi gặp số (là Health).
+    // 3. Đọc Health, Spike, Movement, Set.
+    // 4. Đọc các chuỗi còn lại của dòng -> đó là Positions.
+    in >> m_ID;
+    string namePart, fullName;
+    while (in >> namePart) {
+        if (isdigit(namePart[0])) {
+            // Đây là phần Health.
+            m_Health = stoi(namePart);
+            break;
+        }
+        if (!fullName.empty()) {
+            fullName += " ";
+        }
+        fullName += namePart;
+    }
+    m_Name = fullName;
+    in >> m_BaseSpike >> m_BaseMovement >> m_BaseSet;
+    string position;
+    while (in >> position) {
+        m_Positions.push_back(position);
+    }
+    // Cập nhật trạng thái và stats ban đầu.
+    UpdatePlayerStatsBasedOnHealth();
+}
+class CVollayballTeam{
+    private:
+    vector <CPlayer> m_Players;
+    public:
+    void InputList(string filename);
+    void RemovePlayerByPosition(string position);
+    
+
+}
